7cf3007dffab18d87bea2eb09cc376df52ddaa6b
5Y2 <sedylaurent@gmail.com>
2020-01-03 10:05:16 +0100

    add something to somewhere

---
+++ ProjetTestBis/src/OperateurTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+
+/**
+ * @author MOI
+ *
+ */
+public class OperateurTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int maVariableInt1;
+		maVariableInt1 = 256;
+		System.out.println(maVariableInt1);
+		
+		int maVariableInt2;
+		maVariableInt2 = maVariableInt1;
+	//	System.out.println(maVariableInt2);
+		
+		int maVariableInt3;
+		maVariableInt3 = maVariableInt1 + maVariableInt2;
+		System.out.println(maVariableInt3);
+		
+		
+		// TODO cela est un commentaire?
+		
+
+	}
+
+}
