29fed5426e7bd950a46e83c51aec075c073d038f
5Y2 <sedylaurent@gmail.com>
2020-01-03 08:43:18 +0100

    add TestClass in ProjectTestBis

---
+++ ProjetTestBis/src/ClassTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author CRM
+ *
+ */
+public class ClassTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Au revoir");
+		System.out.println("Mais non, attendez!");
+		System.out.println("Enfin pourquoi?");
+		System.out.println("Pour tester les raccourci clavier allons!");
+		System.out.println("Alors évite au moins les fôtes d'orthogriff");
+		System.out.println("Bein sûr.");
+		System.out.println("...");
+	}
+	
+}
