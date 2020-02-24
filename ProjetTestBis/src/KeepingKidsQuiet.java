445d302e34763769dcafffa60db3f38e7ad678dc
5Y2 <sedylaurent@gmail.com>
2020-02-24 10:44:13 +0100

    add java for dummy line

---
+++ ProjetTestBis/src/KeepingKidsQuiet.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+/**
+ * @author 5Y2
+ *
+ */
+	class KeepingKidsQuiet {
+
+		public static void main(String args[]) {
+		    int gumballs;
+		    int kids;
+		    int gumballsPerKid;
+
+		    gumballs = 30;
+		    kids = 4;
+		    gumballsPerKid = gumballs / kids;
+
+		    System.out.print("Chaque enfant reçoit ");
+		    System.out.print(gumballsPerKid);
+		    System.out.println(" boules de gomme.");
+		    
+		    }
+		}
